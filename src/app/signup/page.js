921dc2267921533a43b02ed6f25c6c0042f578f7
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-11-30 14:48:59 +0800

    Add nextjs initial files. Add signup and signin page

---
+++ src/app/signup/page.js
@@ -0,0 +1,82 @@
+"use client";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="confirm"
+            label="Confirm Password"
+            type="password"
+            id="confirm"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+          <Box>
+            <Link href="/" variant="body2">
+              {"Already have an account? Sign in"}
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
