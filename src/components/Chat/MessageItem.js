785762c263fe50e53c6fa09d8e5d40f1bc95c16c
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-12-01 20:40:57 +0800

    Add date and time

--- src/components/Chat/MessageItem.js
+++ src/components/Chat/MessageItem.js
@@ -6,33 +6,63 @@ import AvatarMaker from "../AvatarMaker";
 import { useAuthContext } from "@/shared/context/AuthContext";
 
 export default function MessageItem({
-  data: { message, sender, recipient } = {},
+  data: { message, sender, recipient, createdAt } = {},
   children,
 }) {
   const { currentUser } = useAuthContext();
   const isOwner = sender?.uid === currentUser?.uid;
 
+  console.log("createdAt", typeof createdAt, createdAt.seconds);
+
+  const postedDate = createdAt?.seconds
+    ? new Date(createdAt.seconds * 1000).toLocaleString()
+    : "";
+
   return (
-    <Box
-      sx={{
-        padding: 1,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: !isOwner ? "flex-start" : "flex-end",
-      }}
-    >
-      {!isOwner && <AvatarMaker name={recipient?.name || ""} marginRight={1} />}
-      <Typography
-        component="span"
-        fontSize={14}
-        margin={0}
-        padding={"7px 20px"}
-        borderRadius={6}
-        color={!isOwner ? "inherit" : "white"}
-        backgroundColor={!isOwner ? grey[200] : blue[500]}
+    <>
+      {postedDate && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: 1,
+          }}
+        >
+          <Typography
+            component="span"
+            fontSize={11}
+            margin={0}
+            borderRadius={6}
+            color={grey[500]}
+          >
+            {new Date(createdAt.seconds * 1000).toLocaleString()}
+          </Typography>
+        </Box>
+      )}
+      <Box
+        sx={{
+          padding: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: !isOwner ? "flex-start" : "flex-end",
+        }}
       >
-        {message || children}
-      </Typography>
-    </Box>
+        {!isOwner && (
+          <AvatarMaker name={recipient?.name || ""} marginRight={1} />
+        )}
+
+        <Typography
+          component="span"
+          fontSize={14}
+          margin={0}
+          padding={"7px 20px"}
+          borderRadius={6}
+          color={!isOwner ? "inherit" : "white"}
+          backgroundColor={!isOwner ? grey[200] : blue[500]}
+        >
+          {message || children}
+        </Typography>
+      </Box>
+    </>
   );
 }
