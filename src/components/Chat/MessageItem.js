a3e821e9044d89d40c1ac99d5e2c406179fe26ae
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-12-02 19:47:43 +0800

    Scroll to last message when new message is added

--- src/components/Chat/MessageItem.js
+++ src/components/Chat/MessageItem.js
@@ -1,3 +1,5 @@
+import { forwardRef } from "react";
+
 import { Typography } from "@mui/material";
 import { grey, blue, black } from "@mui/material/colors";
 import Box from "@mui/material/Box";
@@ -6,65 +8,76 @@ import AvatarMaker from "../AvatarMaker";
 import { useAuthContext } from "@/shared/context/AuthContext";
 import { areDateAndTimeEqual } from "@/shared/lib/utils";
 
-export default function MessageItem({
-  data: { message, sender, recipient, createdAt } = {},
-  prevData,
-  children,
-}) {
-  const { currentUser } = useAuthContext();
-  const isOwner = sender?.uid === currentUser?.uid;
+const MessageItem = forwardRef(
+  (
+    {
+      data: { message, sender, recipient, createdAt } = {},
+      prevData,
+      children,
+    },
+    ref
+  ) => {
+    const { currentUser } = useAuthContext();
+    const isOwner = sender?.uid === currentUser?.uid;
 
-  const postedDate = createdAt?.seconds
-    ? new Date(createdAt.seconds * 1000)
-    : "";
+    const postedDate = createdAt?.seconds
+      ? new Date(createdAt.seconds * 1000)
+      : "";
 
-  const prevPostedDate = prevData?.createdAt?.seconds
-    ? new Date(prevData.createdAt.seconds * 1000)
-    : "";
+    const prevPostedDate = prevData?.createdAt?.seconds
+      ? new Date(prevData.createdAt.seconds * 1000)
+      : "";
 
-  return (
-    <>
-      {postedDate && !areDateAndTimeEqual(postedDate, prevPostedDate) && (
+    return (
+      <>
+        {postedDate && !areDateAndTimeEqual(postedDate, prevPostedDate) && (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              marginBottom: 1,
+            }}
+          >
+            <Typography
+              component="span"
+              fontSize={12}
+              margin={0}
+              borderRadius={6}
+              color={grey[600]}
+            >
+              {postedDate.toLocaleString()}
+            </Typography>
+          </Box>
+        )}
         <Box
+          ref={ref}
           sx={{
+            padding: 1,
             display: "flex",
-            justifyContent: "center",
-            marginBottom: 1,
+            alignItems: "center",
+            justifyContent: !isOwner ? "flex-start" : "flex-end",
           }}
         >
+          {!isOwner && (
+            <AvatarMaker name={sender?.name || ""} marginRight={1} />
+          )}
+
           <Typography
             component="span"
-            fontSize={12}
+            fontSize={14}
             margin={0}
+            padding={"7px 20px"}
             borderRadius={6}
-            color={grey[600]}
+            color={!isOwner ? "inherit" : "white"}
+            backgroundColor={!isOwner ? grey[200] : blue[500]}
           >
-            {postedDate.toLocaleString()}
+            {message || children}
           </Typography>
         </Box>
-      )}
-      <Box
-        sx={{
-          padding: 1,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: !isOwner ? "flex-start" : "flex-end",
-        }}
-      >
-        {!isOwner && <AvatarMaker name={sender?.name || ""} marginRight={1} />}
+      </>
+    );
+  }
+);
 
-        <Typography
-          component="span"
-          fontSize={14}
-          margin={0}
-          padding={"7px 20px"}
-          borderRadius={6}
-          color={!isOwner ? "inherit" : "white"}
-          backgroundColor={!isOwner ? grey[200] : blue[500]}
-        >
-          {message || children}
-        </Typography>
-      </Box>
-    </>
-  );
-}
+MessageItem.displayName = "MessageItem";
+export default MessageItem;
--- src/components/Chat/MessageList.js
+++ src/components/Chat/MessageList.js
@@ -1,14 +1,26 @@
+import { useEffect, useRef } from "react";
+
 import Box from "@mui/material/Box";
 
 import MessageItem from "./MessageItem";
 import useMessages from "@/shared/hooks/firebase/useMessages";
 import CircularProgress from "@mui/material/CircularProgress";
 
+// TODO(Improvement): Implement logic to handle scrolling and load older messages when the
+// user scrolls to the top. Currently, the component loads all messages in the thread all at once.
+
 export default function MessageList({ threadId, sx }) {
+  const scrollRef = useRef();
   const { messages, isLoading } = useMessages(threadId);
+
+  useEffect(() => {
+    if (!messages?.length) return;
+    scrollRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+  }, [messages?.length]);
 
   return (
     <Box
+      ref={scrollRef}
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -50,6 +62,11 @@ export default function MessageList({ threadId, sx }) {
           (data, index) =>
             data?.message && (
               <MessageItem
+                {...(messages.length - 1 === index
+                  ? {
+                      ref: scrollRef,
+                    }
+                  : {})}
                 key={index}
                 data={data}
                 prevData={index > 0 && messages[index - 1]}
