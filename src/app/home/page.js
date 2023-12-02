cfbeb4fb98611ee8683f9a6056421f3fa00288e0
Paul Christian Rueda <nexuslifeline@gmail.com>
2023-12-02 09:42:56 +0800

    clear current user on logout

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,12 +5,15 @@ import { useRouter } from "next/navigation";
 import Chat from "@/components/Chat";
 import withAuth from "@/shared/hoc/withAuth";
 import useFirebaseAuth from "@/shared/hooks/firebase/useFirebaseAuth";
+import { useAuthContext } from "@/shared/context/AuthContext";
 
 const HomePage = () => {
   const { logout } = useFirebaseAuth();
+  const { setCurrentUser } = useAuthContext();
   const router = useRouter();
 
   const handleSignOut = () => {
+    setCurrentUser(null);
     logout();
     router.push("/");
   };
